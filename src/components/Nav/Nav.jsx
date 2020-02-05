e87e73c4eee7c4857b4e890632730ef722c049bf
German Cutraro <germancutraro@hotmail.com>
2020-02-05 20:54:32 -0300

    nav refactor + css module

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import YoutubeLogo from "./images/youtube-logo.png";
-import "./Nav.css";
+import React from 'react';
+import YoutubeLogo from '../../assets/images/youtube-logo.png';
+import styles from './Nav.module.css';
 
-const Nav = props => {
-  return (
-    <div className="menu">
-      <nav className="main-nav">
-        <a href="/">
-          <img src={YoutubeLogo} alt="Youtube Logo" className="youtube-logo" />
-        </a>
-        {props.children}
-      </nav>
-    </div>
-  );
-};
+const Nav = props => (
+  <div className={styles.menu}>
+    <nav className={styles.main_nav}>
+      <a href='/'>
+        <img
+          src={YoutubeLogo}
+          alt='Youtube Logo'
+          className={styles.youtube_logo}
+        />
+      </a>
+      {props.children}
+    </nav>
+  </div>
+);
 
-export default Nav;
+export default React.memo(Nav);
